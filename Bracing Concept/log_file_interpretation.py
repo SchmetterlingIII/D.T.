25d85b67b619090b6dfd5d05cd999f9ad3eefbac
SchmetterlingIII <99736344+SchmetterlingIII@users.noreply.github.com>
2025-04-13 00:17:08 +0100

    took me three days to do basic trig... cambridge isn't for me

--- Bracing Concept/log_file_interpretation.py
+++ Bracing Concept/log_file_interpretation.py
@@ -1,17 +1,51 @@
 import csv
-import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import animation
 
-def fileReadingPractice():
-    with open("sensor_log.csv", mode = "r") as log_file:
-        log_file_read = log_file.read() # read whole file
-        log_file_list = log_file_read.split("\n")
-        log_file_list_000 = log_file_list.pop(0) # removes first instance of list (header: "timestamp,x,y,z")
+"""Issues brought up
+    1. can be more efficient with the csv module I imported
+    2. need to understand this logic about plotting
+    3. reformat this using classes because this will increasinly get messy and I would also want the option of:
+        a. switching between position displaying
+        b. normalised display
+        c. tilt angles
+        d. acceleration values
+        e. critical tilt angles
+        f. maximum acceleration values beyond which we ignore (error handling?)
         
-        for i in log_file_list:
-            coordinate = i.split(",")
-            x = coordinate[1]
-            y = coordinate[2]
-            z = coordinate[3]
+    """ 
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+with open("sensor_log.csv", mode = "r") as log_file:
+    log_file_read = log_file.read() # read whole file
+    log_file_list = log_file_read.split("\n")
+    log_file_list_000 = log_file_list.pop(0) # removes first instance of list (header: "timestamp,x,y,z")
+    
+    x = []
+    y = []
+    z = []
+    
+    for i in log_file_list:
+        coordinate = i.split(",")
+        
+        if len(coordinate) >= 4:
+            try:
+                x.append(float(coordinate[1]))
+                y.append(float(coordinate[2]))
+                z.append(float(coordinate[3]))
+                
+                # getting the tilts angles from these, in radians
             
-        
-fileReadingPractice()
+            except ValueError:
+                print("Input {i} is invalid") # an almost redundant prompt
+
+        ax.scatter(x,y,z)
+        plt.pause(0.1)  
+    plt.show()
+                
+
