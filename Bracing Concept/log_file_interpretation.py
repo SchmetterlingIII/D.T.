50044b65c146d6e006ea9bc24f39aef5787680db
SchmetterlingIII <99736344+SchmetterlingIII@users.noreply.github.com>
2025-04-14 00:12:53 +0100

    i am not an idiot??

--- Bracing Concept/log_file_interpretation.py
+++ Bracing Concept/log_file_interpretation.py
@@ -34,7 +34,7 @@ class AccelerometerData:
         self.data_loaded = False
         
     def load_data(self):
-        with open("sensor_log.csv", mode = "r") as log_file:
+        with open(self.file_path, mode = "r") as log_file:
             log_file_read = log_file.read() # read whole file
             log_file_list = log_file_read.split("\n")
             
@@ -86,7 +86,14 @@ class AccelerometerData:
             self.unit_vector.append(acc_unit_vector)
             
         return self
+    
 
+test = AccelerometerData("sensor_log.csv")
+test.load_data()
+
+
+
+"""
 class AccelerometerVisualiser:
     def __init__(self, data):
         self.data = data
@@ -102,46 +109,9 @@ ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
-with open("sensor_log.csv", mode = "r") as log_file:
-    log_file_read = log_file.read() # read whole file
-    log_file_list = log_file_read.split("\n")
-    log_file_list_000 = log_file_list.pop(0) # removes first instance of list (header: "timestamp,x,y,z")
-    
-    x = []
-    y = []
-    z = []
-    
-    for i in log_file_list:
-        coordinate = i.split(",")
-        
-        if len(coordinate) >= 4:
-            try:
-                local_x = float(coordinate[1])
-                local_y = float(coordinate[2])
-                local_z = float(coordinate[3])
-                
-                x.append(local_x)
-                y.append(local_y)
-                z.append(local_z)
-                
-                # getting the tilts angles from these, in radians
-                local_tilt_xz = math.atan2(local_z, local_x)
-                local_tilt_xy = math.atan2(local_y, local_x)
-                local_tilt_zy = math.atan2(local_z, local_y)
-                
-                print(f"-- {local_tilt_xz}, {local_tilt_xy}, {local_tilt_zy} --")
-            
-            except ValueError:
-                print("Input {i} is invalid") # an almost redundant prompt
-                                
-                                
-
-"""
 
         ax.scatter(x,y,z)
         plt.pause(0.1)  
     plt.show()
-    
-"""
-                
+"""         
 
