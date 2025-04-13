4ff4f20584f8c0c34e48c0841aed0c2af1ad7a5a
SchmetterlingIII <99736344+SchmetterlingIII@users.noreply.github.com>
2025-04-14 00:08:17 +0100

    it acc wasn't that bad (with the classes)

--- Bracing Concept/log_file_interpretation.py
+++ Bracing Concept/log_file_interpretation.py
@@ -21,15 +21,71 @@ import math
 
 
 class AccelerometerData:
-    def __init__(self, filePath):
-        self.filePath =
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.timestamps = []
+        self.x = []
+        self.y = []
+        self.z = []
+        self.tilt_xz = []
+        self.tilt_xy = []
+        self.tilt_zy = []
+        self.unit_vector = []
+        self.data_loaded = False
         
     def load_data(self):
-        
+        with open("sensor_log.csv", mode = "r") as log_file:
+            log_file_read = log_file.read() # read whole file
+            log_file_list = log_file_read.split("\n")
+            
+            r_header = log_file_list.pop(0) # remove header (i.e. "timestamps, x, y, z")
+           
+            for i in log_file_list:
+                coordinate = i.split(",")
+                
+                if len(coordinate) >= 4:
+                    try:
+                        timestamp = float(coordinate[0])
+                        local_x = float(coordinate[1])
+                        local_y = float(coordinate[2])
+                        local_z = float(coordinate[3])
+                        
+                        self.timestamps.append(timestamp)
+                        self.x.append(local_x)
+                        self.y.append(local_y)
+                        self.z.append(local_z)
+                     
+                    
+                    except ValueError:
+                        print("Input {i} is invalid") # an almost redundant prompt
+                        
+        self.data_loaded = True                           
+        return self                                
+
     def calculate_tilt_angles(self):
+        if not self.data_loaded:
+            self.load_data()
+            
+        for i in range(len(self.x)):
+            self.tilt_xz.append(math.atan2(self.z[i], self.x[i]))
+            self.tilt_xy.append(math.atan2(self.y[i], self.x[i]))
+            self.tilt_zy.append(math.atan2(self.z[i], self.y[i]))
+            
+        return self
         
     def normalise_data(self):
-
+        if not self.data_loaded:
+            self.load_data()
+            
+        
+        for i in range(len(self.x)):
+            acc_unit_vector = []
+            mag_acc = math.hypot(self.x, self.y, self.z)
+            unit_x, unit_y, unit_z = self.x/mag_acc, self.y/mag_acc, self.z/mag_acc
+            acc_unit_vector.append(unit_x, unit_y_, unit_z)
+            self.unit_vector.append(acc_unit_vector)
+            
+        return self
 
 class AccelerometerVisualiser:
     def __init__(self, data):
@@ -77,7 +133,9 @@ with open("sensor_log.csv", mode = "r") as log_file:
             
             except ValueError:
                 print("Input {i} is invalid") # an almost redundant prompt
-    
+                                
+                                
+
 """
 
         ax.scatter(x,y,z)
